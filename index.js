37c2dad8cfe1ddb4d359550dd4183cb9f3055809
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2022-11-01 10:12:04 -0400

    f

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const DOMSelectors = {};
+const DOMSelectors = {
+  button: document.getElementById("btn"),
+  text: document.querySelector("#test"),
+  point: document.querySelectorAll(".point"),
+  pointTwo: document.getElementById(".pointTwo"),
+};
+
+function backgroundAndText(background, text) {}
