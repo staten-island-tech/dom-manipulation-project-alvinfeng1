f197595959f94998ac33e6a2266722a83c760af6
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2022-11-02 10:13:01 -0400

    f

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const DOMSelectors = {
-  button: document.getElementById("btn"),
-  text: document.querySelector("#test"),
-  point: document.querySelectorAll(".point"),
-  pointTwo: document.getElementById(".pointTwo"),
-};
+// const DOMSelectors = {
+//   button: document.getElementById("btn"),
+//   text: document.querySelector("#test"),
+//   points: document.querySelectorAll(".point"),
+//   box: document.getElementById("big-black-box"),
+// };
 
-function backgroundAndText(background, text) {}
+// function backgroundAndText(background, text) {
+//   background.style.backgroundColor = "red";
+//   text.innerHTML = "this is know a big red box";
+// }
+
+// backgroundAndText(DOMSelectors.box, DOMSelectors.text);
+const qsa = (els) => document.querySelectorAll(els);
+const allColors = qsa(".color");
+
+console.log(allColors);
+
+const badColor = allColors.filter((node) => { if(node.classList[1] ===)})
