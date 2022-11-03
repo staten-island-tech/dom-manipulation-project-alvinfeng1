39acc0cf63a60a57db37538c678e1becea2853f0
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2022-11-03 09:56:05 -0400

    g

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-// const DOMSelectors = {
-//   button: document.getElementById("btn"),
-//   text: document.querySelector("#test"),
-//   points: document.querySelectorAll(".point"),
-//   box: document.getElementById("big-black-box"),
-// };
+const DOMSelectors = {
+  button: document.getElementById("btn"),
+  text: document.querySelector("#test"),
+  points: document.querySelectorAll(".point"),
+  box: document.getElementById("big-black-box"),
+};
 
-// function backgroundAndText(background, text) {
-//   background.style.backgroundColor = "red";
-//   text.innerHTML = "this is know a big red box";
-// }
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = "red";
+  text.textContent = "this is know a big red box";
+  text.style.fontSize = "40px";
+}
 
-// backgroundAndText(DOMSelectors.box, DOMSelectors.text);
-const qsa = (els) => document.querySelectorAll(els);
-const allColors = qsa(".color");
+// const qsa = (els) => document.querySelectorAll(els);
+// const allColors = qsa(".color");
 
-console.log(allColors);
+// console.log(allColors);
 
-const badColor = allColors.filter((node) => { if(node.classList[1] ===)})
+// const allColorsArray = Array.from(allColors);
+
+// const goodColor = allColorsArray.filter((node) => {
+//   if (node.classList[1] === "good") {
+//     return true;
+//   }
+//   return false;
+// });
+// console.log(goodColor);
+
+// const badArray = Array.from(qsa(".bad"));
+// const bad = badArray.map((item) => item.innerHTML);
+// console.log(bad);
