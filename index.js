f0fd88425beb847cf6c50f9c22ee1aaeff337c79
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2022-11-14 10:12:50 -0500

    g

--- index.js
+++ index.js
@@ -3,14 +3,21 @@ const DOMSelectors = {
   text: document.querySelector("#text"),
   box: document.getElementById("box"),
   points: document.querySelectorAll(".point"),
+  imput: document.querySelector("#input"),
+  form: document.querySelectorAll("#form"),
 };
 
-function styling(document, text) {
-  document.body.style.backgroundColor = "red";
-  text.textContent = "this is a different colored box";
-  text.style.fontSize = "40px";
-  text.style.color = "white";
-}
+DOMSelectors.button.addEventListener("sumbit", function () {
+  Event.preventDefault(refresh);
+  let imput = DOMSelectors.box.value;
+  console.log(imput);
+});
+// function styling(document, text) {
+//   document.body.style.backgroundColor = "red";
+//   text.textContent = "this is a different colored box";
+//   text.style.fontSize = "40px";
+//   text.style.color = "white";
+// }
 
 // function changeLi() {
 //   let pointIndex = 1;
