d45b897c37b791e4d7aaeb5cbd3acc54bd00e8f1
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2022-11-15 10:12:18 -0500

    f

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const DOMSelectors = {
   button: document.getElementById("btn"),
-  text: document.querySelector("#text"),
-  box: document.getElementById("box"),
-  points: document.querySelectorAll(".point"),
-  imput: document.querySelector("#input"),
-  form: document.querySelectorAll("#form"),
+  box: document.getElementById("#text-box"),
+  form: document.querySelectorAll("form"),
 };
 
 DOMSelectors.button.addEventListener("sumbit", function () {
@@ -12,3 +9,5 @@ DOMSelectors.button.addEventListener("sumbit", function () {
   let imput = DOMSelectors.box.value;
   console.log(imput);
 });
+
+console.log;
