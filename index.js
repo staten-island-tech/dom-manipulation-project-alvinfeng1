041a78aa4d856909dd306cba76cb56bcba9b5d04
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2022-11-15 09:56:23 -0500

    t

--- index.js
+++ index.js
@@ -12,39 +12,3 @@ DOMSelectors.button.addEventListener("sumbit", function () {
   let imput = DOMSelectors.box.value;
   console.log(imput);
 });
-// function styling(document, text) {
-//   document.body.style.backgroundColor = "red";
-//   text.textContent = "this is a different colored box";
-//   text.style.fontSize = "40px";
-//   text.style.color = "white";
-// }
-
-// function changeLi() {
-//   let pointIndex = 1;
-//   DOMSelectors.points.forEach((point) => {
-//     point.addEventListener("click", function () {
-//       point.textConent = `hello i am a point ${pointIndex}`;
-//       pointIndex++;
-//     });
-//   });
-// }
-// changeLi();
-
-// const qsa = (els) => document.querySelectorAll(els);
-// const allColors = qsa(".color");
-
-// console.log(allColors);
-
-// const allColorsArray = Array.from(allColors);
-
-// const goodColor = allColorsArray.filter((node) => {
-//   if (node.classList[1] === "good") {
-//     return true;
-//   }
-//   return false;
-// });
-// console.log(goodColor);
-
-// const badArray = Array.from(qsa(".bad"));
-// const bad = badArray.map((item) => item.innerHTML);
-// console.log(bad);
