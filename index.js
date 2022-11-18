c376da6b7e83ad1d2122b6c2a4a6b12831461155
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2022-11-18 10:12:07 -0500

    f

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
 const DOMSelectors = {
-  button: document.getElementById("btn"),
-  box: document.getElementById(".text-box"),
-  form: document.querySelectorAll("form"),
-  removeButton: document.getElementById("remove-btn"),
-  display: document.getElementById("display-img"),
-  formInput: document.querySelectorAll("#form-input")
+  button: document.getElementById(`btn`),
+  box: document.getElementById(`.text-box`),
+  form: document.querySelectorAll(`form`),
+  removeButton: document.getElementById(`remove-btn`),
+  display: document.getElementById(`display-img`),
+  formInput: document.querySelectorAll(`#form-input`),
+  flavor: document.getElementById(`flavor`),
+  type: document.getElementById(`type`),
+  brand: document.getElementById(`brand`),
+  resetButton: document.getElementById(`resetbutton`),
 };
 
-function getValues() {
-  console.log($('#form-imput').serializeArray());
-  Event.preventDefalut()
-}
+// function getValues() {
+//   console.log($("#form-imput").serializeArray());
+//   Event.preventDefalut();
+// }
 
-DOMSelectors.button.addEventListener("click", function () {
+DOMSelectors.button.addEventListener("submit", function () {
   e.preventDefault();
-  let everything = document.querySelectorAll("form-imput") 
+  // let everything = document.querySelectorAll("form-imput");
+  // console.log(everything);
   let imputs = Array.box;
+  snack.type = imputs[1].value;
+  snack.flavor = imputs[2].value;
+  snack.brand = imputs[3].value;
   console.log(imputs);
-  snack.type = imputs[1].value
-  snack.flavor = imputs[2].value
-  snack.brand = imputs[3].value
-  console.log(everything)
 });
+
+DOMSelectors.resetButton.addEventListener("submit", function () {
+  e.preventDefault();
+  Document.getElementById("form").reset();
+});
+
+function display() {
+  DOMSelectors.display.insertAdjacentHTML(
+    "beforeend",
+    `
+    <h2>${snack.type}</h2>
+    <br>
+    <h2>${snack.flavor}</h2>
+    <br>
+    <h2>${snack.brand}</h2>
+    <br>
+    `
+  );
+}
