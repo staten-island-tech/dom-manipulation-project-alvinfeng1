9a338c382210b8f28fd362f2553e33916b627fa3
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2022-11-18 00:05:14 -0500

    j

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const DOMSelectors = {
   form: document.querySelectorAll("form"),
   removeButton: document.getElementById("remove-btn"),
   display: document.getElementById("display-img"),
+  formInput: document.querySelectorAll("#form-input")
 };
 
-// DOMSelectors.button.addEventListener("button", function () {
-//   e.preventDefault();
-//   let imput = DOMSelectors.box.value;
-//   //create object from values
-//   console.log(imput);
-// });
-
-function logValues() {
+function getValues() {
   console.log($('#form-imput').serializeArray());
-  Event.preventDefalut(logValues)
+  Event.preventDefalut()
 }
+
+DOMSelectors.button.addEventListener("click", function () {
+  e.preventDefault();
+  let everything = document.querySelectorAll("form-imput") 
+  let imputs = Array.box;
+  console.log(imputs);
+  snack.type = imputs[1].value
+  snack.flavor = imputs[2].value
+  snack.brand = imputs[3].value
+  console.log(everything)
+});
