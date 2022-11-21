3a19f5ab2e3c2616992c5a9d837b8dbb7d676162
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2022-11-21 10:12:35 -0500

    f

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const DOMSelectors = {
-  button: document.getElementById(`btn`),
-  box: document.getElementById(`.text-box`),
-  form: document.querySelectorAll(`form`),
-  removeButton: document.getElementById(`remove-btn`),
-  display: document.getElementById(`display-img`),
-  formInput: document.querySelectorAll(`#form-input`),
-  flavor: document.getElementById(`flavor`),
-  type: document.getElementById(`type`),
-  brand: document.getElementById(`brand`),
-  resetButton: document.getElementById(`resetbutton`),
+  button: document.getElementById("btn"),
+  form: document.getElementById("form"),
+  removeButton: document.getElementById("remove-btn"),
+  display: document.getElementById("display-card"),
+  formInput: document.querySelectorAll("#form-input"),
+  resetButton: document.getElementById("resetbutton"),
+  typeInput: document.getElementById("type-input"),
+  flavorInput: document.getElementById("flavor-input"),
+  brandInput: document.getElementById("brand-input"),
 };
 
 // function getValues() {
@@ -17,35 +16,53 @@ const DOMSelectors = {
 // }
 
 DOMSelectors.button.addEventListener("submit", function () {
-  e.preventDefault();
+  Event.preventDefault();
   // let everything = document.querySelectorAll("form-imput");
   // console.log(everything);
   let imputs = Array.box;
-  snack.type = imputs[1].value;
-  snack.flavor = imputs[2].value;
-  snack.brand = imputs[3].value;
+  typeInput = imputs[1].value;
+  flavorInput = imputs[2].value;
+  brandInput = imputs[3].value;
   console.log(imputs);
 });
 
-DOMSelectors.resetButton.addEventListener("submit", function () {
-  Document.getElementById("form").reset();
-});
+function resetbutton() {
+  DOMSelectors.brandInput.value = ``;
+  DOMSelectors.flavorInput.value = ``;
+  DOMSelectors.typeInput.value = ``;
+}
 
 function display() {
+  h1 = DOMSelectors.typeInput.value;
+  h2 = DOMSelectors.flavorInput.value;
+  h3 = DOMSelectors.brandInput.value;
   DOMSelectors.display.insertAdjacentHTML(
-    "beforeend",
-    `
-    <h2>${snack.type}</h2>
+    "afterbegin",
+    `<div class = "display-card"> 
+    <h1>${h1}</h1>
     <br>
-    <h2>${snack.flavor}</h2>
+    <h2>${h2}</h2>
     <br>
-    <h2>${snack.brand}</h2>
+    <h3>${h3}</h3>
     <br>
-    `
+    <button class = "remove-btn"> Remove</button>
+    </div>
+       `
   );
 }
 
-  DOMSelectors.removeButton.addEventListener("removebuttton", function () {
-    let image = DOMSelectors.display.value;
-    delete image
-  });
+// function destroy() {
+//   const btn = Array.form(document.getElementByClassName("remove-button"));
+//   btn.forEach((button) => {
+//     button.addEventListener("click", function () {
+//       this.parentElement.remove;
+//     });
+//   });
+// }
+
+DOMSelectors.form.addEventListener("submit", function () {
+  event.preventDefault();
+  // destroy();
+  display();
+  resetbutton();
+});
